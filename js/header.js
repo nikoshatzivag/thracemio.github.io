26db2f0e844aa2c4860f83cab068bf971c07dcba
gublerakos <mariaapntz@gmail.com>
2021-05-16 22:35:02 +0300

    logo + icon updated on every page

--- js/header.js
+++ js/header.js
@@ -4,11 +4,11 @@ document.write("<a href=\"javascript:void(0);\" class=\"icon\" onclick=\"myFunct
 document.write("<i class=\"fa fa-bars\"><\/i>");
 document.write("<\/a>");
 document.write("<a class=\"titleLink\" href=\"..\/index.html\">");
-document.write("<img class=\"titleLogo\" src=\"..\/logo.png\" alt=\"Thrace MIO\">");
+document.write("<img class=\"titleLogo\" src=\"..\/logos\/logo1000.png\" alt=\"Thrace MIO\">");
 document.write("<div class=\"titleText\">");
 document.write("<h1>Thracing on MIC<\/h1>");
 document.write("<h4>Model International Crises<\/h4>");
 document.write("<\/div>");
 document.write("<\/a>");
 document.write("<\/header>");
-// } 
+// }
